6eb6ca89887a65dae890be5f33b197e4903105ec
lluuvvii <kurniawanluvi64@gmail.com>
2024-10-23 20:27:09 +0700

    create model course

---
+++ src/models/courseStatistic.js
@@ -0,0 +1,10 @@
+import mongoose from 'mongoose';
+
+const CourseStatisticSchema = new mongoose.Schema({
+  courseId: { type: mongoose.Schema.Types.ObjectId, ref: 'Course', required: true },
+  userId: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
+  completed: { type: Boolean, default: false },
+  score: { type: Number, default: 0 },
+});
+
+export default mongoose.models.CourseStatistic || mongoose.model('CourseStatistic', CourseStatisticSchema);
